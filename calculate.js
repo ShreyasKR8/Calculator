b43e5a171aad928e78243d691f663196729e81ba
Shreyaskr8 <skrravi08@gmail.com>
2023-10-08 00:25:32 +0530

    add basic calculations

--- calculate.js
+++ calculate.js
@@ -1,89 +1,106 @@
-
-// const expression = prompt("Enter expression to calculate: ");
-
-// console.log(expression);
-
-// function calculate()
-// {
-//     if(expression.includes('+'))
-//     {
-//         const operatorIndex = expression.lastIndexOf('+')
-//         operandA = parseInt(expression.slice(0, operatorIndex));
-//         operandB = parseInt(expression.slice(-(operatorIndex + 1)));
-//         add(operandA, operandB);
-//         return;
-//     }
-    
-//     if(expression.includes('-'))
-//     {
-//         const operatorIndex = expression.indexOf('-');
-//         if(operatorIndex === 0)
-//         {
-//             operandA = parseInt(expression.slice(0, operatorIndex));
-//             operatorIndex = expression.lastIndexOf('-');
-//             operandB = parseInt(expression.slice(-(operatorIndex + 1)));
-//             subtract(operandA, operandB);
-//             return;
-//         }
-//         operatorIndex = expression.lastIndexOf('-');
-//         operandA = parseInt(expression.slice(0, operatorIndex));
-//         operandB = parseInt(expression.slice(-(operatorIndex + 1)));
-//         subtract(operandA, operandB);
-//         return;
-//     }
-
-//     if(expression.includes('*'))
-//     {
-//         const operatorIndex = expression.lastIndexOf('*')
-//         operandA = parseInt(expression.slice(0, operatorIndex));
-//         operandB = parseInt(expression.slice(-operatorIndex));
-//         multiply(operandA, operandB);
-//         return;
-//     }
-//     if(expression.includes('/'))
-//     {
-//         const operatorIndex = expression.lastIndexOf('/')
-//         operandA = parseInt(expression.slice(0, operatorIndex));
-//         operandB = parseInt(expression.slice(-operatorIndex));
-//         divide(operandA, operandB);
-//         return;
-//     }
-// } 
-
-// function add(operandA, operandB)
-// {
-//     console.log(operandA + operandB);
-//     console.log(operandA);
-//     console.log(operandB);
-// }
-
-// function subtract(operandA, operandB)
-// {
-//     console.log(operandA - operandB);
-//     console.log(operandA); 
-//     console.log(operandB);
-// }
-
-// function multiply(operandA, operandB)
-// {
-//     console.log(operandA * operandB);
-// }
-
-// function divide(operandA, operandB)
-// {
-//     console.log(operandA / operandB);
-// }
- 
-// calculate();
-
 let operandA = 0;
 let operandB = 0;
-
 const buttons = document.querySelectorAll("button");
 const displayScreen = document.querySelector(".calculator-display");
+let str = "wer"
+console.log(str.slice(0, -1));
+function calculate(expression)
+{
+    if(expression.includes('+'))
+    {
+        return add(expression);
+    }
+    
+    if(expression.includes('-'))
+    {
+        const operatorIndex = expression.indexOf('-');
+        // if(operatorIndex === 0)
+        // {
+        //     operandA = -parseInt(expression.slice(0, operatorIndex));
+        //     // operatorIndex = expression.lastIndexOf('-');
+        //     operandB = parseInt(expression.slice(operatorIndex));
+        //     // let result = subtract(operandA, operandB);
+        //     // return
+        // }
+        // operatorIndex = expression.lastIndexOf('-');
+        operandA = parseInt(expression.slice(0, operatorIndex));
+        operandB = parseInt(expression.slice(-operatorIndex));
+        // operandB = parseInt(expression.slice(-(operatorIndex + 1)));
+        let result = subtract(operandA, operandB);
+        return result;
+        
+    }
+
+    if(expression.includes('*'))
+    {
+        return multiply(expression);
+    }
+
+    if(expression.includes('/'))
+    {
+        return divide(expression);
+    }
+
+    if(expression.includes('%'))
+    {
+        return modulo(expression);
+    }
+} 
+
+function add(expression)
+{
+    const operatorIndex = expression.lastIndexOf('+')
+    operandA = parseInt(expression.slice(0, operatorIndex));
+    operandB = parseInt(expression.slice(-(operatorIndex + 1)));
+    return (operandA + operandB);
+}
+
+function subtract(operandA, operandB)
+{
+    return (operandA - operandB);
+}
+
+function multiply(expression)
+{
+    const operatorIndex = expression.lastIndexOf('*')
+    operandA = parseInt(expression.slice(0, operatorIndex));
+    operandB = parseInt(expression.slice(-operatorIndex));
+    return (operandA * operandB);
+}
+
+function divide(expression)
+{
+    const operatorIndex = expression.lastIndexOf('/')
+    operandA = parseInt(expression.slice(0, operatorIndex));
+    operandB = parseInt(expression.slice(-operatorIndex));
+    if(operandA === 0)
+        return "0, I mean its alright"
+    if(operandB === 0)
+        return "dude seriously??";
+    return (operandA / operandB);
+}
+
+function modulo(expression)
+{
+    const operatorIndex = expression.lastIndexOf('%')
+    operandA = parseInt(expression.slice(0, operatorIndex));
+    operandB = parseInt(expression.slice(-operatorIndex));
+    if(operandB === 0)
+        return "dude seriously??";
+    return (operandA % operandB);
+}
 
 buttons.forEach(button => {
     button.addEventListener("click", (e) => {
-        displayScreen.textContent += button.textContent;
+        if(button.textContent == "AC")
+            displayScreen.textContent = "";
+        else if(button.textContent == "+/-")
+        displayScreen.textContent += "-";
+    else if(button.textContent != "=")
+    displayScreen.textContent += button.textContent;
+    else if(button.textContent === "=")
+    {
+        displayScreen.textContent = calculate(displayScreen.textContent);
+    }
     })
 });
